2c9f092bd1430754ff2f19ca719f5d9518cd96d2
Sergio Ordine <sjordine@yahoo.com.br>
2021-08-23 10:39:23 -0300

    Adding list view code

--- DogBreeds/DogBreeds/Controller/ListViewController.swift
+++ DogBreeds/DogBreeds/Controller/ListViewController.swift
@@ -13,11 +13,74 @@ class ListViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    enum Section: String {
+        case main
     }
     
-
+    var dataSource: UICollectionViewDiffableDataSource<Section, DogBreed>! = nil
+    var dogServices = DogServices()
+    
+    //MARK:- View Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureHierarchy()
+        configureDataSource()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //Updates the doog breed list
+        dogServices.breeds { result in
+            if case let .success(breeds) = result {
+                var snapshot = NSDiffableDataSourceSnapshot<Section, DogBreed>()
+                snapshot.appendSections([.main])
+                snapshot.appendItems(breeds, toSection: .main)
+                //The data source update must be done in the main queue
+                OperationQueue.main.addOperation {
+                    self.dataSource.apply(snapshot, animatingDifferences: true)
+                }
+            }
+        }
+    }
+    
+    //MARK: - Collection View layout
+    
+    private func configureHierarchy() {
+        collectionView.collectionViewLayout = createLayout()
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        //Defines a plain list layout
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+    
+    //MARK: - Data Source configuration
+    
+    private func configureDataSource() {
+        
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, DogBreed> { (cell, indexPath, item) in
+            var content = cell.defaultContentConfiguration()
+            content.text = item.name
+            cell.contentConfiguration = content
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, DogBreed>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: DogBreed) -> UICollectionViewCell? in
+            
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
+                                                                for: indexPath,
+                                                                item: identifier)
+        }
+        
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, DogBreed>()
+        snapshot.appendSections([.main])
+        dataSource.apply(snapshot, animatingDifferences: false)
+        
+    }
+    
 }
+
