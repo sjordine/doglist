a1b2f1e32cef0791b07b9922b57ef68a6eb1154e
Sergio Ordine <sjordine@yahoo.com.br>
2021-08-23 10:05:30 -0300

    Create ListViewController.swift

---
+++ DogBreeds/DogBreeds/Controller/ListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListViewController.swift
+//  DogBreeds
+//
+//  Created by SERGIO J RAFAEL ORDINE on 23/08/21.
+//
+
+import UIKit
+
+
+/// View Controller for a list (Table view like) collection view
+class ListViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
